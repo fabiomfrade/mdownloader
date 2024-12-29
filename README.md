1bca0b2d384d83a5cdacdb00adb3a92923bd550f
Fabio Frade <fabiomfrade@gmail.com>
2024-12-28 21:13:15 -0300

    Fazendo atualização de versão usando funções

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ O objetivo deste projeto é permitir o download de músicas extraídas de vídeo
 O propósito deste projeto é meramente educacional. Enquanto aprimoro conhecimentos da linguagem Python, vou melhorando o código criado aqui e mantendo as versões anteriores em histórico de ``commits`` e/ou ``tags`` do projeto.
 
 ---
-
+## Copyrights mantidos pelos donos dos vídeos pesquisados
 ### ___Este projeto está sendo desenvolvido e este README tende a mudar constantemente com a evolução do projeto___
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = "0.1.6"
+__version__ = "0.2.0"
 __author__ = "Fábio Frade - fabiomfrade@gmail.com"
 __license__ = "unlicensed"
 
@@ -15,30 +15,37 @@ from youtubesearchpython import VideosSearch
 characters = [127932, 127929, 9989, 11093]
 
 # Variáveis obrigatórias
-resp = ""
+resp = None
 # download = ".download_links.tmp"
+
+# Funções do script
+def buscar_video(musica):
+    busca = VideosSearch(musica, limit=1)
+    resultado = busca.result()
+    video = resultado['result'][0]
+    titulo = video['title']
+    url = video['link']
+    print(f"Encontrado vídeo {titulo}")
+    print("Aguarde enquanto o download é feito...")
+    baixar_audio(url)
+#   # Armazenando URL em arquivo temporário
+#   with open(download, "a") as arquivo:
+#   arquivo.write(f"{url}\n")
+
+def baixar_audio(url):
+    with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3', 'quiet': True}) as audio:
+        info_dict = audio.extract_info(url, download = True)
+        video_title = info_dict['title']
+        print("MP3 baixado com sucesso\n")
+
 
 # Inicio do Script
 print("{:^50}".format(f"Music Downloader - {__version__} - {chr(characters[random.randrange(0,2)])}"))
-
 while True:
-    resp = input(f"Digite o nome da música a baixar \nDigite 'sair' para encerrar: ")
-    if resp.lower() == "sair" or resp == "":
-        break
+    resp = input(f"Digite o nome da música a baixar\nDigite 'sair' para encerrar: ")
+    if resp.lower() == "sair" or not resp:
+        sys.exit(0)
     else:
-        busca = VideosSearch(resp, limit=1)
-        resultado = busca.result()
-        try:
-            # Obtendo valores do vídeo encontrado
-            video = resultado['result'][0]
-            titulo = video['title']
-            url = video['link']
-            print(f"Baixando MP3 de {titulo}, aguarde...")
-            # Baixando o MP3
-            with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3', 'quiet': True}) as audio:
-                info_dict = audio.extract_info(url, download = True)
-                video_title = info_dict['title']
-                print("MP3 baixado com sucesso\n")
-        except:
-            print("Nenhum Video encontrado")
-            sys.exit(1)
+        buscar_video(resp)
+
+
